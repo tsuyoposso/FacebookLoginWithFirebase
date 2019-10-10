884f4648a1d866fca7954c67a642866734633c68
長坂豪士 <nagasakatsuyoshi@nagasakagoushinoMacBook-Air.local>
2019-10-10 14:37:58 +0900

    commit complete

--- FacebookLoginWithFirebase/FacebookLoginWithFirebase/AppDelegate.swift
+++ FacebookLoginWithFirebase/FacebookLoginWithFirebase/AppDelegate.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+
+
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,9 +20,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        
         return true
     }
 
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return ApplicationDelegate.shared
+        .application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+        
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+        AppEvents.activateApp()
+    }
+    
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
